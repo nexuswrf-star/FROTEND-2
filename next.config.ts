e28f23e26a7e51ebd67ef3ceb16d9eb7112f3037
Z User <z@container>
2026-04-16 15:07:01 +0000

    e720d280-170c-4a47-8cc8-05a586e2f4c1

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  experimental: {
-    allowedDevOrigins: [
-      'space.z.ai',
-      /.+\.space\.z\.ai$/,
-    ],
-  },
 };
 
 export default nextConfig;
